c0cd4d0b0e215e65367c0f1864db141c1e309554
nexd <nexd@0333ffd2-54e2-0310-be41-f3f467a15db8>
2005-10-30 22:05:15 +0000

    initial commit.

---
+++ xpcms/lib/creole/test/classes/creole/drivers/mysql/MySQLResultSetTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'creole/ResultSetTest.php';
+
+/**
+ * MySQLResultSet tests.
+ * 
+ * 
+ * @author Hans Lellelid <hans@xmpl.org>
+ * @version $Revision: 1.1 $
+ */
+class MySQLResultSetTest extends ResultSetTest {
+        
+}
